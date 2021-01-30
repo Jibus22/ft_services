fd466572692b920292319026e7efec90bfa90d90
Le Corre <jibeguitare@hotmail.fr>
2021-01-31 00:12:26 +0100

    echo $'foo' in bash

--- setup.sh
+++ setup.sh
@@ -56,6 +56,6 @@ kubectl apply -f srcs/manifests/nginx-deployment.yaml
 kubectl apply -f srcs/manifests/grafana-deployment.yaml
 
 
-echo "\tPODS" && kubectl get po -o wide && echo "---\n\n\tDEPLOYMENTS" && kubectl get deploy -o wide && echo "---\n\n\tSERVICES" && kubectl get svc -o wide
+echo $'"\tPODS' && kubectl get po -o wide && echo $'---\n\n\tDEPLOYMENTS' && kubectl get deploy -o wide && echo $'---\n\n\tSERVICES' && kubectl get svc -o wide
 
 minikube dashboard
